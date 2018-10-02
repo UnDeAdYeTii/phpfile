939e2e5c54883b8d4d798d6c11a96f44ce911e53
pxgamer <owzie123@gmail.com>
2018-10-02 10:44:28 +0100

    Update the src directory files to use PSR-2

--- src/File.php
+++ src/File.php
@@ -1,9 +1,16 @@
 <?php
+
 namespace YeTii\PhpFile;
+
 /**
  * Class File
  */
-class File {
+class File
+{
+    /**
+     * @var string
+     */
+    protected $compiled;
     /**
      * @var string
      */
@@ -24,17 +31,21 @@ class File {
      * @var int
      */
     protected $spaces = 4;
+
     /**
      * File constructor.
      * @param $file
      */
-    public function __construct($file) {
+    public function __construct($file)
+    {
         $this->file = $file;
     }
+
     /**
      * @return $this
      */
-    public function line() {
+    public function line()
+    {
         foreach (func_get_args() as $arg) {
             if ($this->line !== '') {
                 $this->lines[] = $this->pad().$this->line;
@@ -44,19 +55,23 @@ class File {
         }
         return $this;
     }
+
     /**
      * @return $this
      */
-    public function break() {
+    public function break()
+    {
         $this->lines[] = '';
         return $this;
     }
+
     /**
      * @param string $arg
-     * @param bool $submit
+     * @param bool   $submit
      * @return $this
      */
-    public function string($arg, $submit = false) {
+    public function string($arg, $submit = false)
+    {
         $this->line .= $arg;
         if ($submit) {
             $this->lines[] = $this->pad().$this->line;
@@ -64,13 +79,15 @@ class File {
         }
         return $this;
     }
+
     /**
-     * @param string $arg
+     * @param string       $arg
      * @param mixed|string $if
-     * @param bool $submit
+     * @param bool         $submit
      * @return $this
      */
-    public function stringIf($arg, $if, $submit = false) {
+    public function stringIf($arg, $if, $submit = false)
+    {
         if ($if !== null && $if !== [] && $if !== '') {
             $this->string($arg);
         }
@@ -80,29 +97,35 @@ class File {
         }
         return $this;
     }
+
     /**
      * @return string
      */
-    public function compile() {
+    public function compile()
+    {
         return implode("\n", $this->lines);
     }
+
     /**
-     * @param string $arg
+     * @param string       $arg
      * @param mixed|string $if
      * @return $this
      */
-    public function lineIf($arg, $if) {
+    public function lineIf($arg, $if)
+    {
         if ($if !== null && $if !== [] && $if !== '') {
             $this->line($arg);
         }
         return $this;
     }
+
     /**
      * @param array|string|mixed $arg
-     * @param mixed|string $if
+     * @param mixed|string       $if
      * @return $this
      */
-    public function foreachIf($arg, $if) {
+    public function foreachIf($arg, $if)
+    {
         if ($if !== null && $if !== [] && $if !== '') {
             foreach ($arg as $a) {
                 $this->line($a);
@@ -110,32 +133,40 @@ class File {
         }
         return $this;
     }
+
     /**
      * @param int $indent
      * @return $this
      */
-    public function indent(int $indent) {
+    public function indent(int $indent)
+    {
         $this->indent = abs($indent);
         return $this;
     }
+
     /**
      * @param int $spaces
      * @return $this
      */
-    public function spaces(int $spaces) {
+    public function spaces(int $spaces)
+    {
         $this->spaces = abs($spaces);
         return $this;
     }
+
     /**
      * @return string
      */
-    public function pad() {
-        return str_pad('', $this->indent*$this->spaces, ' ', STR_PAD_LEFT);
+    public function pad()
+    {
+        return str_pad('', $this->indent * $this->spaces, ' ', STR_PAD_LEFT);
     }
+
     /**
      * @return $this
      */
-    public function write() {
+    public function write()
+    {
         $this->compiled = $this->compile();
         file_put_contents($this->file, $this->compiled);
         return $this;
