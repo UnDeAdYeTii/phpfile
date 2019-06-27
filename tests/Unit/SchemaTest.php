6968ab2f04609a770b5623f43bc615a5bac84b4d
Owen Voke <owzie123@gmail.com>
2019-06-27 13:35:30 +0100

    Move unit tests to a Unit directory

--- tests/Unit/SchemaTest.php
+++ tests/Unit/SchemaTest.php
@@ -2,37 +2,37 @@
 
 declare(strict_types=1);
 
-namespace YeTii\PhpFile\Tests;
+namespace YeTii\PhpFile\Tests\Unit;
 
-use RuntimeException;
 use YeTii\PhpFile\Schematic;
 use PHPUnit\Framework\TestCase;
 use YeTii\PhpFile\Schema\PhpClass;
+use YeTii\PhpFile\Exception\InvalidSchemaException;
 
 final class SchemaTest extends TestCase
 {
     /** @test */
     public function itThrowsAnExceptionOnNonExistantConfigurationFile(): void
     {
-        $this->expectException(RuntimeException::class);
+        $this->expectException(InvalidSchemaException::class);
         $this->expectExceptionMessage('Configuration schema file not found');
 
-        Schematic::makeFromConfiguration(__DIR__.'/schemas/non-existant-file.neon');
+        Schematic::makeFromConfiguration(__DIR__.'/../schemas/non-existant-file.neon');
     }
 
     /** @test */
     public function itThrowsAnExceptionOnInvalidFileExtension(): void
     {
-        $this->expectException(RuntimeException::class);
+        $this->expectException(InvalidSchemaException::class);
         $this->expectExceptionMessage('Invalid configuration schema file extension');
 
-        Schematic::makeFromConfiguration(__DIR__.'/schemas/invalid.txt');
+        Schematic::makeFromConfiguration(__DIR__.'/../schemas/invalid.txt');
     }
 
     /** @test */
     public function itCreatesAPhpClassInstanceFromANeonSchemaFile(): void
     {
-        $schema = Schematic::makeFromConfiguration(__DIR__.'/schemas/valid.neon');
+        $schema = Schematic::makeFromConfiguration(__DIR__.'/../schemas/valid.neon');
 
         $this->assertInstanceOf(PhpClass::class, $schema->getData());
     }
@@ -40,7 +40,7 @@ final class SchemaTest extends TestCase
     /** @test */
     public function itCreatesAPhpClassInstanceFromAJsonSchemaFile(): void
     {
-        $schema = Schematic::makeFromConfiguration(__DIR__.'/schemas/valid.json');
+        $schema = Schematic::makeFromConfiguration(__DIR__.'/../schemas/valid.json');
 
         $this->assertInstanceOf(PhpClass::class, $schema->getData());
     }
